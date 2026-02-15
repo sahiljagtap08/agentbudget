0e5e8740b6214d793a8bfcb26f67a4ed5a790f44
Sahil Jagtap <sahil.jagtap45@gmail.com>
2026-02-15 12:37:13 -0500

    Add AsyncBudgetSession to module exports for improved session handling

--- agentbudget/__init__.py
+++ agentbudget/__init__.py
@@ -4,11 +4,12 @@ __version__ = "0.1.0"
 
 from .budget import AgentBudget
 from .exceptions import AgentBudgetError, BudgetExhausted, InvalidBudget
-from .session import BudgetSession, LoopDetected
+from .session import AsyncBudgetSession, BudgetSession, LoopDetected
 
 __all__ = [
     "AgentBudget",
     "AgentBudgetError",
+    "AsyncBudgetSession",
     "BudgetExhausted",
     "BudgetSession",
     "InvalidBudget",
